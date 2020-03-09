9669da12ede2d2b80e362629fa80b528108873ec
Alexander Talberg <alexandermtalberg@gmail.com>
2020-03-08 21:44:35 -0600

    did some styling on  the nav bar, got the meal plan to print out the correct info and in 7 diffent col

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,83 +1,86 @@
-import React, { useState, useReducer,useEffect } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import API from "../../utils/API"
+import "./styles.css"
 
 const Example = (props) => {
   const [collapsed, setCollapsed] = useState(true);
-  const [user ,setUser] = useState({
-      loggedIn:false
+  const [user, setUser] = useState({
+    loggedIn: false
 
   })
   function logout() {
-    API.logout().then((data)=> {
-        window.location.pathname = "/"
-    }).catch((err)=> {
-        console.log(err)
+    API.logout().then((data) => {
+      window.location.pathname = "/"
+    }).catch((err) => {
+      console.log(err)
     })
-}
+  }
 
   useEffect(() => {
     API.isLoggedIn().then(user => {
-        if (user.data.loggedIn) {
-            setUser({
-                loggedIn: true,
-                user: user.data.user
-            });
-            console.log(user.data)
-        }
+      if (user.data.loggedIn) {
+        setUser({
+          loggedIn: true,
+          user: user.data.user
+        });
+        console.log(user.data)
+      }
     },
-        )
-}, [])
+    )
+  }, [])
 
 
   const toggleNavbar = () => setCollapsed(!collapsed);
 
   return (
     <div>
-      <Navbar className=" text-light  " color="primary" light>
-          <hr/>
-        <NavbarBrand href="/" className=" text-light  mr-auto ">Working Title</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className=" text-light mr-2 ml-auto" />
+      <Navbar onClick={toggleNavbar} className=" font  text-light   " color="danger" secondary>
+        <hr />
+        <NavbarBrand onClick={toggleNavbar} className=" font  text-dark title   mr-auto "><h1>Working Title</h1></NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className=" font  text-light ml-auto" />
         <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+          <Nav className=" font bg-light rounded" navbar>
+            
             <NavItem>
-              <NavLink className="text-light" href="/home">Home <hr/></NavLink>
+              
+              <NavLink className=" font  bg-secondary rounded  pl-5 text-light" href="/home"><br></br>Home <hr /></NavLink>
 
             </NavItem>
             <NavItem>
-              <NavLink className=" text-light" href="/cookbook">Cook Book <hr/></NavLink>
+              <NavLink className=" font  pl-5  text-secondary" href="/cookbook"><br></br>Cook Book <hr /></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className=" text-light" href="/mealplan">Meal Plan <hr/></NavLink>
+              <NavLink className=" font  pl-5 bg-secondary rounded text-light" href="/mealplan"><br></br>Meal Plan <hr /></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className=" text-light" href="/mealplan/create">Create Meal Plan <hr/></NavLink>
+              <NavLink className=" font   pl-5  text-secondary" href="/mealplan/create"><br></br>Create Meal Plan <hr /></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className=" text-light" href="/addrecipe">Add Recipe <hr/></NavLink>
+              <NavLink className=" font  pl-5 bg-secondary rounded text-light" href="/addrecipe"><br></br>Add Recipe <hr /></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className=" text-light" href="/recipesearch">Recipe Search<hr/></NavLink>
+              <NavLink className=" font  pl-5  text-secondary" href="/recipesearch"><br></br>Recipe Search<hr /></NavLink>
             </NavItem>
-            { !user.loggedIn ? (<>
-            <NavItem>
-              <NavLink className=" text-light" href="/login">Log In<hr/></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className=" text-light" href="/signup">Sign Up <hr/></NavLink>
-            </NavItem></>
-             ) :(
-            <NavItem>
-              <NavLink className=" text-light" onClick={logout}>Log Out<hr/></NavLink>
-            </NavItem> )
-            
-        
-        }  
+            {!user.loggedIn ? (<>
+              <NavItem>
+                <NavLink className=" font  pl-5 bg-secondary rounded  text-light" href="/login"><br></br>Log In<hr /></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className=" font  pl-5  text-secondary" href="/signup"><br></br>Sign Up <hr /></NavLink>
+              </NavItem></>
+            ) : (
+                <NavItem>
+                  <NavLink className=" font  pl-5   text-light" onClick={logout}><br></br>Log Out<hr /></NavLink>
+                </NavItem>)
+
+
+            }
 
           </Nav>
         </Collapse>
       </Navbar>
-      <hr/>
+      <hr />
     </div>
   );
 }
